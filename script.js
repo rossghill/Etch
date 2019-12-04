9b0538dcfc97ef5d41bf1263b13b98406361296a
Ross Hill <ross.hill@nucleusfinancial.com>
2019-12-04 15:09:50 +0000

    Logic for building object and retrieving random image

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var models = Object.create({});
+
+    var asos_m_count = 31;
+    var asos_f_count = 22;
+
+    for (var x = 0; x <= (asos_m_count + asos_f_count); x++) {
+        if(x>0 && x <= asos_m_count){
+            models[x] = { key: x, brand: "Asos", gender: "male", imageURL: "/imgs/asos-model-imgs/male/" + x + ".jpg" };
+        }
+        // if (x >= asos_f_count){
+        //     models[x] = { key: x, brand: "Asos", gender: "female", imageURL: "/imgs/asos-model-imgs/female/" + (x+1) + ".jpg" };
+        // }
+        else {
+            models[x] = { key: x + 1, brand: "Asos", gender: "female", imageURL: "/imgs/asos-model-imgs/female/" + (x - asos_f_count - 9) + ".jpg" }
+        }
+    }
+
+    // console.table(models);
+
+    let imageContainer = document.getElementById("random-img");
+    let totalEntries = asos_m_count + asos_f_count;
+    let randomNo = (Math.random() * (totalEntries - 0 + 1)) << 0;
+
+    for (key in models) {
+        if (models.hasOwnProperty(key)) {
+            var value = models[key];
+            if (value.key === randomNo) {
+                console.log(value.imageURL);
+            }
+        }
+    }
+
+
+
+})
