6a184c659efb74e7a8d8ef4e15c52d1ae4597747
Ross Hill <ross.hill@nucleusfinancial.com>
2019-12-11 11:16:48 +0000

    Random images now showing based on male/female selection, unsplash models added

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-function generateModel(){
+document.addEventListener('DOMContentLoaded', function () {
 
-    console.log("hi");
+    let source = document.getElementById("source_data").innerText;
+    let gender = document.getElementById("gender_data").innerText;
+    let time = document.getElementById("time_data").innerText;
+
+    console.log(time);
 
     var models = Object.create({});
-
     var asos_m_count = 31;
     var asos_f_count = 22;
 
-    for (var x = 0; x <= (asos_m_count + asos_f_count); x++) {
-        if(x>0 && x <= asos_m_count){
-            models[x] = { key: x, brand: "Asos", gender: "male", imageURL: "/imgs/asos-model-imgs/male/" + x + ".jpg" };
-        }
-        else {
-            models[x] = { key: x + 1, brand: "Asos", gender: "female", imageURL: "/imgs/asos-model-imgs/female/" + (x - asos_f_count - 9) + ".jpg" }
+    if (source === "ASOS") {
+        for (var x = 0; x <= (asos_m_count + asos_f_count); x++) {
+            if (x > 0 && x <= asos_m_count) {
+                models[x] = { key: x, brand: "Asos", gender: "male", imageURL: "/imgs/asos-model-imgs/male/" + x + ".jpg" };
+            }
+            else {
+                models[x] = { key: x + 1, brand: "Asos", gender: "female", imageURL: "/imgs/asos-model-imgs/female/" + (x - asos_f_count - 9) + ".jpg" }
+            }
         }
     }
-
 
     let imageContainer = document.getElementById("random-img");
     let totalEntries = asos_m_count + asos_f_count;
@@ -25,9 +29,8 @@ function generateModel(){
         if (models.hasOwnProperty(key)) {
             var value = models[key];
             if (value.key === randomNo) {
-                console.log(value.imageURL);
                 imageContainer.setAttribute("src", value.imageURL);
             }
         }
     }
-}
+})
--- sketchScreen.php
+++ sketchScreen.php
@@ -11,11 +11,27 @@
         <script type="text/javascript" src="script.js"></script>
 
 </head>
-<body onload="generateModel()">
+
+<body>
+
+<div class="formdata">
+    <div id="source_data">
+        <?php $source = htmlspecialchars($_POST['selectSource']); echo $source; ?>
+    </div>
+    <div id="gender_data">
+        <?php $source = htmlspecialchars($_POST['selectGender']); echo $source; ?>
+    </div>
+    <div id="time_data">
+        <?php $source = htmlspecialchars($_POST['selectTime']); echo $source; ?>
+    </div>
+    
+</div>    
+    
 
     <div class="img-container">
         <img id="random-img" src="#" />
     </div>
-    
+<script src="/script.js"></script>    
 </body>
+
 </html>
