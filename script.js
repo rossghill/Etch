71dbd5f8e550c2ba26591d3e69d086857c04ef9b
rhill <rhill@squiz.co.uk>
2020-06-03 14:06:05 +0100

    Replaced php with js form parsing

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ document.addEventListener('DOMContentLoaded', function () {
     // Get form values from html url
     function getQueryVariable(variable)
     {
-           let query = window.location.search.substring(1);
-           let vars = query.split("&");
-           for (var i=0;i<vars.length;i++) {
-                   var pair = vars[i].split("=");
-                   if(pair[0] == variable){return pair[1];}
-           }
-           return(false);
+        let query = window.location.search.substring(1);
+        let vars = query.split("&");
+        for (var i=0;i<vars.length;i++) {
+                var pair = vars[i].split("=");
+                if(pair[0] == variable){return pair[1];}
+        }
+        return(false);
     }
 
     let maleQuantity = m.length;
